b128df00d3ed2b0448e6524ec8004c8f585291ef
lucienvb <lucienvanbussel@gmail.com>
2025-11-20 10:57:43 +0100

    project setup, types and id generator

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-struct Order {
-    id: u64,
-    price: f64,
-    quantity: u64,
-    is_buy: bool
+mod orderbook;
+
+use chrono::{DateTime, Utc};
+use core::pin::Pin;
+use orderbook::types::{Order, OrderId, Side};
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::time::SystemTime;
+
+static ORDER_ID_COUNTER: AtomicU64 = AtomicU64::new(1);
+
+pub fn next_order_id() -> OrderId {
+    OrderId(ORDER_ID_COUNTER.fetch_add(1, Ordering::Relaxed))
 }
 
 fn main() {
-    let order1 = Order {id :1, price: 15.0, quantity: 5, is_buy: true};
-    let id = order1.id;
-    let price = order1.price;
-    let quantity = order1.quantity;
-    let is_buy = order1.is_buy;
-    print!("id: {id}, price: {price}, quantity: {quantity}, is_buy: {is_buy}\n");
+    let order1 = Order {
+        id: next_order_id(),
+        price: 15.3,
+        quantity: 5,
+        side: Side::Bid,
+        timestamp: SystemTime::now(),
+    };
+    let order2 = Order {
+        id: next_order_id(),
+        price: 25.3,
+        quantity: 8,
+        side: Side::Ask,
+        timestamp: SystemTime::now(),
+    };
+    {
+        let price = order1.price;
+        let quantity = order1.quantity;
+        let side = order1.side == Side::Bid;
+        let timestamp: DateTime<Utc> = order1.timestamp.into();
+        println!("ORDER 1 --> id: ..., price: {price}, quantity: {quantity}, side: {side}, timestamp: {}\n", {timestamp.format("%d/%m/%Y %T")});
+    }
+    {
+        let price = order2.price;
+        let quantity = order1.quantity;
+        let side = order2.side == Side::Bid;
+        let timestamp: DateTime<Utc> = order2.timestamp.into();
+        println!("ORDER 2 --> id: ..., price: {price}, quantity: {quantity}, side: {side}, timestamp: {}\n", {timestamp.format("%d/%m/%Y %T")});
+    }
 }
---
+++ src/orderbook/types.rs
@@ -0,0 +1,22 @@
+use std::time::SystemTime;
+
+pub type Price = f64;
+pub type Quantity = u64;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct OrderId(pub u64);
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Side {
+    Bid,
+    Ask,
+}
+
+#[derive(Debug, Clone)]
+pub struct Order {
+    pub id: OrderId,
+    pub side: Side,
+    pub price: Price,
+    pub quantity: Quantity,
+    pub timestamp: SystemTime,
+}
